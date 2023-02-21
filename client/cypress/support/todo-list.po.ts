efd1e63a27a30947330b13ffdc1143d699e4dcc0
Ash <plase024@morris.umn.edu>
2023-02-21 16:57:44 -0600

    Add end to end tests for filtering todos by name

---
+++ client/cypress/support/todo-list.po.ts
@@ -0,0 +1,25 @@
+export class TodoListPage {
+  navigateTo() {
+    return cy.visit('/todos');
+  }
+
+  /**
+   * Gets the title of the app when visiting the `/todos` page.
+   *
+   * @returns the value of the element with the ID `.todo-list-title`
+   */
+  getTodoTitle() {
+    return cy.get('.todo-list-title');
+  }
+
+  /**
+   * Get all the `app-todo-card` DOM elements. This will be
+   * empty if we're using the list view of the users.
+   *
+   * @returns an iterable (`Cypress.Chainable`) containing all
+   *   the `app-user-card` DOM elements.
+   */
+   getTodoCards() {
+    return cy.get('.todo-cards-container app-todo-card');
+  }
+}
