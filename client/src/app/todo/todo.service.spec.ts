36fc5ad81c7c6d0265bd85a0feb35dbbdea46f12
Ash <plase024@morris.umn.edu>
2023-02-20 18:13:12 -0600

    Add tests for `todo` Service

--- client/src/app/todo/todo.service.spec.ts
+++ client/src/app/todo/todo.service.spec.ts
@@ -1,3 +1,5 @@
+import { HttpClient } from '@angular/common/http';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
 
 import { TodoService } from './todo.service';
@@ -5,9 +7,19 @@ import { TodoService } from './todo.service';
 describe('TodoService', () => {
   let service: TodoService;
 
+  // These are used to mock the HTTP requests so that we (a) don't have to
+  // have the server running and (b) we can check exactly which HTTP
+  // requests were made to ensure that we're making the correct requests.
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(TodoService);
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule]
+    });
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    service = new TodoService(httpClient);
   });
 
   it('should be created', () => {
