2d8e5549e52712bf5eb0b3a7625e2bb3e56367cb
Ash <plase024@morris.umn.edu>
2023-02-22 13:31:07 -0600

    Add server tests for `TodoController`

---
+++ server/src/test/java/umm3601/todo/TodoControllerSpec.java
@@ -0,0 +1,180 @@
+package umm3601.todo;
+
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import com.mongodb.MongoClientSettings;
+import com.mongodb.ServerAddress;
+import com.mongodb.client.MongoClient;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+
+import org.bson.Document;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import io.javalin.http.Context;
+import io.javalin.http.HttpStatus;
+
+/**
+ * Tests the logic of the UserController
+ *
+ * @throws IOException
+ */
+// The tests here include a ton of "magic numbers" (numeric constants).
+// It wasn't clear to me that giving all of them names would actually
+// help things. The fact that it wasn't obvious what to call some
+// of them says a lot. Maybe what this ultimately means is that
+// these tests can/should be restructured so the constants (there are
+// also a lot of "magic strings" that Checkstyle doesn't actually
+// flag as a problem) make more sense.
+@SuppressWarnings({ "MagicNumber" })
+public class TodoControllerSpec {
+
+  // An instance of the controller we're testing that is prepared in
+  // `setupEach()`, and then exercised in the various tests below.
+  private TodoController todoController;
+
+  // The client and database that will be used
+  // for all the tests in this spec file.
+  private static MongoClient mongoClient;
+  private static MongoDatabase db;
+
+  @Mock
+  private Context ctx;
+
+  @Captor
+  private ArgumentCaptor<ArrayList<Todo>> todoArrayListCaptor;
+
+  @Captor
+  private ArgumentCaptor<Todo> todoCaptor;
+
+  @Captor
+  private ArgumentCaptor<Map<String, String>> mapCaptor;
+
+  /**
+   * Sets up (the connection to the) DB once; that connection and DB will
+   * then be (re)used for all the tests, and closed in the `teardown()`
+   * method. It's somewhat expensive to establish a connection to the
+   * database, and there are usually limits to how many connections
+   * a database will support at once. Limiting ourselves to a single
+   * connection that will be shared across all the tests in this spec
+   * file helps both speed things up and reduce the load on the DB
+   * engine.
+   */
+  @BeforeAll
+  public static void setupAll() {
+    String mongoAddr = System.getenv().getOrDefault("MONGO_ADDR", "localhost");
+
+    mongoClient = MongoClients.create(
+        MongoClientSettings.builder()
+            .applyToClusterSettings(builder -> builder.hosts(Arrays.asList(new ServerAddress(mongoAddr))))
+            .build()
+    );
+    db = mongoClient.getDatabase("test");
+  }
+
+  @AfterAll
+  public static void teardown() {
+    db.drop();
+    mongoClient.close();
+  }
+
+  @BeforeEach
+  public void setupEach() throws IOException {
+    // Reset our mock context and argument captor (declared with Mockito annotations @Mock and @Captor)
+    MockitoAnnotations.openMocks(this);
+
+    // Setup database
+    MongoCollection<Document> todoDocuments = db.getCollection("todos");
+    todoDocuments.drop();
+    List<Document> testTodos = new ArrayList<>();
+    testTodos.add(
+        new Document()
+            .append("owner", "Blanche")
+            .append("status", false)
+            .append("body", "In sunt ex non tempor cillum commodo amet incididunt anim qui commodo quis. Cillum non labore ex sint esse.")
+            .append("category", "software design"));
+
+    testTodos.add(
+      new Document()
+          .append("owner", "Fry")
+          .append("status", false)
+          .append("body", "Ipsum esse est ullamco magna tempor anim laborum non officia deserunt veniam commodo. Aute minim incididunt ex commodo.")
+          .append("category", "video games"));
+
+    testTodos.add(
+      new Document()
+          .append("owner", "Fry")
+          .append("status", true)
+          .append("body", "Ullamco irure laborum magna dolor non. Anim occaecat adipisicing cillum eu magna in.")
+          .append("category", "homework"));
+
+
+    todoDocuments.insertMany(testTodos);
+
+    todoController = new TodoController(db);
+  }
+
+  @Test
+  public void cangetAllTodos() throws IOException {
+    // When something asks the (mocked) context for the queryParamMap,
+    // it will return an empty map (since there are no query params in this case where we want all users)
+    when(ctx.queryParamMap()).thenReturn(Collections.emptyMap());
+
+    // Now, go ahead and ask the userController to getUsers
+    // (which will, indeed, ask the context for its queryParamMap)
+    todoController.getTodos(ctx);
+
+    // We are going to capture an argument to a function, and the type of that argument will be
+    // of type ArrayList<User> (we said so earlier using a Mockito annotation like this):
+    // @Captor
+    // private ArgumentCaptor<ArrayList<User>> userArrayListCaptor;
+    // We only want to declare that captor once and let the annotation
+    // help us accomplish reassignment of the value for the captor
+    // We reset the values of our annotated declarations using the command
+    // `MockitoAnnotations.openMocks(this);` in our @BeforeEach
+
+    // Specifically, we want to pay attention to the ArrayList<User> that is passed as input
+    // when ctx.json is called --- what is the argument that was passed? We capture it and can refer to it later
+    verify(ctx).json(todoArrayListCaptor.capture());
+    verify(ctx).status(HttpStatus.OK);
+
+    // Check that the database collection holds the same number of documents as the size of the captured List<User>
+    assertEquals(db.getCollection("todos").countDocuments(), todoArrayListCaptor.getValue().size());
+  }
+
+  @Test
+  public void canGetTodosByName() throws IOException {
+    Map<String, List<String>> queryParams = new HashMap<>();
+    queryParams.put(TodoController.OWNER_KEY, Arrays.asList(new String[] {"Fry"}));
+    when(ctx.queryParamMap()).thenReturn(queryParams);
+    when(ctx.queryParam(TodoController.OWNER_KEY)).thenReturn("Fry");
+
+    todoController.getTodos(ctx);
+
+    verify(ctx).json(todoArrayListCaptor.capture());
+    verify(ctx).status(HttpStatus.OK);
+
+    for (Todo todo : todoArrayListCaptor.getValue()) {
+      assertEquals("Fry", todo.owner);
+    }
+  }
+}
